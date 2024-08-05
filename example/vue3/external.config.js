45c2829d907b8323b9e991095ccc9c77eb975821
jianbing.chen <jianbing.chen@huolala.cn>
2024-08-05 17:31:40 +0800

    fix: 修复问题

--- example/vue3/external.config.js
+++ example/vue3/external.config.js
@@ -1,6 +1,6 @@
 export default {
   vue: {
-    src: 'https://static.huolala.cn/npm/vue@3.3.4/dist/vue.runtime.global.prod.js',
+    src: 'https://static.xxx.com/npm/vue@3.3.4/dist/vue.runtime.global.prod.js',
     varName: 'Vue',
     /**
      * 加载位置，默认追加到头部
@@ -11,7 +11,7 @@ export default {
     defer: true,
   },
   'vue-router': {
-    src: 'https://static.huolala.cn/npm/vue-router@4.2.4/dist/vue-router.global.prod.js',
+    src: 'https://static.xxx.com/npm/vue-router@4.2.4/dist/vue-router.global.prod.js',
     varName: 'VueRouter',
     defer: true,
     async: false,
