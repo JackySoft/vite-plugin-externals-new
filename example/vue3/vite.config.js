30c80811f567759c1bf6f8d9c1ae126826d370a3
jianbing.chen <jianbing.chen@huolala.cn>
2024-08-05 19:43:15 +0800

    chore: 替换地址

--- example/vue3/vite.config.js
+++ example/vue3/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     vue(),
     VitePluginExternals({
       vue: {
-        src: 'https://static.huolala.cn/npm/vue@3.3.4/dist/vue.runtime.global.prod.js',
+        src: 'https://static.xxx.com/npm/vue@3.3.4/dist/vue.runtime.global.prod.js',
         varName: 'Vue',
         /**
          * 加载位置，默认追加到头部
@@ -23,7 +23,7 @@ export default defineConfig({
         defer: true,
       },
       'vue-router': {
-        src: 'https://static.huolala.cn/npm/vue-router@4.2.4/dist/vue-router.global.prod.js',
+        src: 'https://static.xxx.com/npm/vue-router@4.2.4/dist/vue-router.global.prod.js',
         varName: 'VueRouter',
         defer: true,
         async: false,
